1b104ecb0fe22ce91b6389adf23c95871a855b78
DESKTOP-21E13UO\dimos <dmaginas@web.de>
2021-11-21 13:49:33 +0100

    Projekt Chuck-A-Luck hinzugefügt.

---
+++ ChuckALuck/ChuckALuck/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChuckALuck
+{
+    class Program
+    {
+			static void Main()
+			{
+				Random rnd = new Random();
+				int runde = 1, kapital = 1000;
+
+				for (;
+					runde <= 1000 && kapital > 0;
+					runde++)
+				{
+
+					kapital--;
+
+					for (int i = 0; i < 3; i++)
+						if (rnd.Next(1, 7) == 6) kapital++;
+				}
+
+				Console.WriteLine("Das Spiel wurde in Runde {0} mit {1} Euro Kapital beendet", --runde, kapital);
+
+				Console.ReadKey(true);
+			}
+		}
+}
