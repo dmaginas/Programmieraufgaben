e1cec41320b6fdec1955bf7401ea379d03a552b3
DESKTOP-21E13UO\dimos <dmaginas@web.de>
2021-11-21 17:27:30 +0100

    Strings 1 hinzugefügt (Vorsicht C# 9.0!)

---
+++ Strings1/Strings1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strings1
+{
+    class Program
+    {
+        static string ValInLen(string s) => $"{(s.Length is < 4 or > 10 ? "NOT " : "")}OKAY ({s.Length})";
+        static void Main(string[] args)
+        {
+            List<string> l = new() { "Pro", "Prog", "Programm", "Programmieren", "Programmieren Lernen" };
+            l.ForEach(x => Console.WriteLine(ValInLen(x)));
+        }
+    }
+}
