c1bd1cfaa2b3ed01d83e1401f8f822cb4276b337
DESKTOP-21E13UO\dimos <dmaginas@web.de>
2021-11-22 19:46:21 +0100

    Added Palindrome

---
+++ Palindrome/CSharp/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindrome
+{
+	class Program
+	{
+		static bool IsPalindrome(string text)
+		{
+			return text == new String(text.Reverse().ToArray());
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(IsPalindrome("ingirumimusnocteetconsumimurigni"));
+		}
+	}
+
+}
