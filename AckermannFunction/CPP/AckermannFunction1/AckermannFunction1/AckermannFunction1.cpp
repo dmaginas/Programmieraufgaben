4e92641b98aefe313fd7cd91aeb938d0a277590d
DESKTOP-21E13UO\dimos <dmaginas@web.de>
2021-12-04 11:08:52 +0100

    Added Ackermann function

---
+++ AckermannFunction/CPP/AckermannFunction1/AckermannFunction1/AckermannFunction1.cpp
@@ -0,0 +1,33 @@
+// AckermannFunction1.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+
+unsigned int ackermann(unsigned int m, unsigned int n) {
+    if (m == 0) {
+        return n + 1;
+    }
+    if (n == 0) {
+        return ackermann(m - 1, 1);
+    }
+    return ackermann(m - 1, ackermann(m, n - 1));
+}
+
+int main() {
+    for (unsigned int m = 0; m < 4; ++m) {
+        for (unsigned int n = 0; n < 10; ++n) {
+            std::cout << "A(" << m << ", " << n << ") = " << ackermann(m, n) << "\n";
+        }
+    }
+}
+
+// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
