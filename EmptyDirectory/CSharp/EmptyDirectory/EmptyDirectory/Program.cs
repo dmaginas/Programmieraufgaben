4eea03c4cfaee1a83a653a448d5e5716944dbc5a
DESKTOP-21E13UO\dimos <dmaginas@web.de>
2022-10-16 11:00:44 +0200

    Added project EmptyDirectory (C#)
    From RosettaCode (https://rosettacode.org/wiki/Empty_directory):
    
    Starting with a path to some directory, determine whether the directory is empty.
    
    An empty directory contains no files nor subdirectories. With Unix or Windows systems, every directory contains an entry for “.” and almost every directory contains “..” (except for a root directory); an empty directory contains no other entries.

---
+++ EmptyDirectory/CSharp/EmptyDirectory/EmptyDirectory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace EmptyDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string dir in args)
+            {
+                Console.WriteLine("'{0}' {1} empty", dir, IsDirectoryEmpty(dir) ? "is" : "is not");
+            }
+        }
+
+        private static bool IsDirectoryEmpty(string dir)
+        {
+            return (Directory.GetFiles(dir).Length == 0 &&
+                Directory.GetDirectories(dir).Length == 0);
+        }
+    }
+}
